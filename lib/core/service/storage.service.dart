17e1dc1a38156b347a054933c4ae85789d1f436d
Atila Augusto <atasantos@rd.com.br>
2024-06-20 10:14:44 -0300

    adjust for the service works with Map

--- lib/core/service/storage.service.dart
+++ lib/core/service/storage.service.dart
@@ -6,7 +6,7 @@ import 'package:uuid/v7.dart';
 
 class StorageService {
   final _storage = getIt.get<SharedPreferences>();
-  List _colletion = List.empty(growable: true);
+  List<Map<String,dynamic>> _colletion = List.empty(growable: true);
   late String _table;
 
   initdb(String table) {
@@ -21,20 +21,20 @@ class StorageService {
     _sync(_colletion);
   }
 
-  Future<void> update(String id, dynamic item) async {
-    _colletion.removeWhere((find) => find.id == item.id);
+  Future<void> update(String id, Map<String, dynamic> item) async {
+    _colletion.removeWhere((find) => find['id'] == item['id']);
     _colletion.add(item);
     _sync(_colletion);
   }
 
    Future<void> delete(String id) async {
-    _colletion.removeWhere((find) => find.id == id);
+    _colletion.removeWhere((find) => find['id'] == id);
     _sync(_colletion);
   }
 
   findById(String id) {
-    final result = _colletion.firstOrNull((find) => find.id == id);
-    if (result != null) {
+    final result = _colletion.firstWhere((find) => find['id'] == id);
+    if (result.isNotEmpty) {
       return result;
     }
     throw Exception("Object not found");
